4b81835713ce83d8f6b7bbbc0969b7b677fb7746
yogithesymbian <yogirenbox33@gmail.com>
2019-07-30 03:52:32 -0400

    init-wait

--- shop/modal-register-user-admin.php
+++ shop/modal-register-user-admin.php
@@ -6,7 +6,7 @@
         <button type="button" class="close" data-dismiss="modal">&times;</button>
 
         <!-- Modal content-->
-        <div class="modal-content modal-style-digitalent" style="width: 430px; height: 590px">
+        <div class="modal-content modal-style-digitalent" style="width: 430px; height: 0px">
 
             <p style="background: white">
                 at this time page of register admin and user is one page,
@@ -14,7 +14,17 @@
                 you got 777 chmod , that's mean you can logged as administrator
                 and as user.
 
-                <a href="../authentication/_register_check_email.php" class="sign-in button"> REGISTER </a>
+                <a href="../authentication/_register_check_email.php" class="sign-in button"> REGISTER <small> as user </small> </a>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde natus facilis error. Accusantium vitae autem repellendus placeat. Ipsam in sapiente atque praesentium, repudiandae magnam soluta dolores eligendi ex quisquam laboriosam?
+            </p>
+
+            <p style="background: white">
+                at this time page of register admin and user is one page,
+                and the role user still maintenance , so if u have register
+                you got 777 chmod , that's mean you can logged as administrator
+                and as user.
+
+                <a href="#" class="sign-in button" data-toggle="tooltip" data-placement="bottom" title="Coming Soon"> REGISTER <small> as admin </small> </a>
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde natus facilis error. Accusantium vitae autem repellendus placeat. Ipsam in sapiente atque praesentium, repudiandae magnam soluta dolores eligendi ex quisquam laboriosam?
             </p>
         </div>
